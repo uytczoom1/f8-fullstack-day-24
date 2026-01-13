a850b9d917a453ed49cde17d15209bfb5f5d9f5d
Truong Cong Uy <uytcf8@fullstack.edu.vn>
2026-01-13 19:21:50 +0700

    update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const isPrime = (n) => {
     if(typeof n !== "number" || n <= 1) return false;
-    for(let i = 2; i < n; i++){
+    for(let i = 2; i <= Math.sqrt(n); i++){
         if(n % i === 0) return false;
     }
     return true;
@@ -14,7 +14,7 @@ const isPrime = (n) => {
 const isPerfectNumber = (n) => {
     if(typeof n !== "number" || n <= 0) return false;
     let sum = 0;
-    for(let i = 1; i < n; i++){
+    for(let i = 1; i <= n / 2; i++){
         if(n % i === 0) sum += i;
     }
     return sum === n;
