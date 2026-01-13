9caa792bb9ce43352c2d1e42ae5c31c35606367d
Truong Cong Uy <uytcf8@fullstack.edu.vn>
2026-01-13 17:17:26 +0700

    Submit

---
+++ main.js
@@ -0,0 +1,21 @@
+// Exercise 1: Check for a Prime Number
+
+const isPrime = (n) => {
+    if(typeof n !== "number" || n <= 1) return false;
+    for(let i = 2; i < n; i++){
+        if(n % i === 0) return false;
+    }
+    return true;
+}
+
+
+// Exercise 2: Check for a Perfect Number
+
+const isPerfectNumber = (n) => {
+    if(typeof n !== "number" || n <= 0) return false;
+    let sum = 0;
+    for(let i = 1; i < n; i++){
+        if(n % i === 0) sum += i;
+    }
+    return sum === n;
+}
